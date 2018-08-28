3aae8b09dd316185a24afbad6d2565395bbff3f9
Prakhar Mittal <mprakhar1705@gmail.com>
2018-08-28 19:23:18 +0530

    bug fixes

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,20 @@
 'use strict'
 const functions = require('firebase-functions');
-const {dialogflow} = require('actions-on-google');
+const DialogflowApp = require('actions-on-google').DialogflowApp;
 const admin = require('firebase-admin');
-
-const app = dialogflow({debug: true});
+//const app = dialogflow({debug: true});
 admin.initializeApp(functions.config().firebase);
 
+
+exports.medicineExpiryTracker= functions.https.onRequest( (request,response) => {
+    const  app = new DialogflowApp({request: request, response: response});
+   
 const db = admin.firestore();
 var data = [];
 
 app.intent('Default Welcome Intent',(conv) =>{
 
-    conv.ask(new SimpleResponse({
-        speech: 'Welcome to Medicine Expiry Tracker. I can help you to track Medicines and their Expiry Date. Following commands are available: Add medicine , Delete Medicine, Get expiry Date for the Medicine, List all medicine. What would you like me to do?',
-        text: 'Welcome to Medicine Expiry Tracker. I can help you to track Medicines and their Expiry Date. Following commands are available: Add medicine , Delete Medicine, Get expiry Date for the Medicine, List all medicine. What would you like me to do?',
-      }));    
+    conv.ask('Welcome to Medicine Expiry Tracker. I can help you to track Medicines and their Expiry Date. Following commands are available: Add medicine , Delete Medicine, Get expiry Date for the Medicine, List all medicine. What would you like me to do?');    
 });
 app.intent('addMedicine',(conv,{medName,date}) =>{
 
@@ -33,4 +33,5 @@ app.intent('addMedicine',(conv,{medName,date}) =>{
     });
 });
 
-exports.medicineExpiryTracker= functions.https.onRequest(app);
+//exports.medicineExpiryTracker= functions.https.onRequest(app);
+});
